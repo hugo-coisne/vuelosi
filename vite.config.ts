a1639830e4b18fbd6b13f1192254decf4bd109a6
hugo_coisne <coisnehugo@gmail.com>
2024-11-06 11:51:51 -0500

    :wrench: silence vite/scss warning

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler' // or "modern"
+        quietDeps: true,
+        api: 'modern-compiler', // or "modern"
       }
     }
   }
