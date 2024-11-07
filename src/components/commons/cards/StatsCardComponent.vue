d13062a2a35de19c615a4db752685f3a5444e21d
hugo_coisne <coisnehugo@gmail.com>
2024-11-07 00:06:14 -0500

    :sparkles: stats card component

---
+++ src/components/commons/cards/StatsCardComponent.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { gsap } from 'gsap'
+import { onMounted, ref, useTemplateRef } from 'vue'
+
+interface Props {
+  context: string
+  stat: number
+}
+
+const props = defineProps<Props>()
+
+const base = ref(0)
+const stats = useTemplateRef('stats')
+let tmp = 0
+
+function animateValue(targetValue: number): void {
+  gsap.to('.stats', {
+    duration: 2,
+    ease: 'linear',
+    onUpdate: () => {
+      tmp = gsap.utils.interpolate(tmp, targetValue, 0.05)
+      base.value = Math.round(tmp)
+    },
+    onComplete: () => {
+      base.value = targetValue
+    },
+  })
+}
+
+onMounted(() => {
+  if (stats.value) {
+    animateValue(props.stat)
+  }
+})
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-infos">
+      <h2 ref="stats" class="stats">{{ base }}</h2>
+      <div class="context">{{ props.context }}</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use '@/assets/css/variables';
+@use '@/assets/css/icons';
+
+.card-infos {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: linear-gradient(135deg, variables.$dark-blue, variables.$blue);
+  height: 165px;
+  border-radius: 15px;
+}
+
+.card {
+  min-width: 231px !important;
+  padding: 0 !important;
+  width: auto;
+}
+
+h2 {
+  color: white;
+  text-align: center;
+  font-size: xxx-large;
+}
+
+.context {
+  color: variables.$yellow;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
