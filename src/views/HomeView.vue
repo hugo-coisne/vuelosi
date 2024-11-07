dd50eaaae6c220c2a20f8efc4006d52ff2c7ab8c
hugo_coisne <coisnehugo@gmail.com>
2024-11-07 17:54:04 -0500

    :white_check_mark: add figures section to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import FiguresComponent from '@/components/home/FiguresComponent.vue'
 import IntroComponent from '@/components/home/IntroComponent.vue'
 </script>
 
 <template>
   <IntroComponent />
+  <FiguresComponent webTitle="TALOSI EN QUELQUES CHIFFRES" mobileTitle="EN QUELQUES CHIFFRES"/>
 </template>
 
 <style lang="scss">
